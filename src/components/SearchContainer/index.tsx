a0ea6c7f6dea2548c1a939ec9fb3d135c4e8e5e7
Gustavo Ragazzi <ragazzi@hotmail.com.br>
2023-05-26 19:38:39 -0300

    feat onSubmit function for files url

--- src/components/SearchContainer/index.tsx
+++ src/components/SearchContainer/index.tsx
@@ -1,11 +1,26 @@
 import styled from "styled-components"
 
+const Container = styled.form`
+    width: 50%;
+    padding: 2em;
+    height: 20px;
+`
+
 export default function SearchContainer() {
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        
+        const files = Array.from(event.target.elements.fileInput.files) as File[];
+
+        files.map((file: File) => (
+            console.log(`Arquivo selecionado: ${file.path}`)
+        ))
+    };
+
     return (
-        <div>
-            <input></input>
-            <button></button>
-            <button></button>
-        </div>
+        <Container className="input-group mb-3" onSubmit={handleSubmit}>
+            <input type="file" className="form-control" id="inputGroupFile02" name="fileInput" multiple/>
+            <button className="input-group-text" type="submit">Adicionar</button>
+        </Container>
     )
 }
