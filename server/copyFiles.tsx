4d47cb6ee5523ed4ea302ccaca79b86a654e0ec2
Gustavo Ragazzi <ragazzi@hotmail.com.br>
2023-05-26 21:35:58 -0300

    output url moved to config.json

--- server/copyFiles.tsx
+++ server/copyFiles.tsx
@@ -1,13 +1,15 @@
 import { ipcMain } from "electron";
 import fs from "fs";
 import path from "path";
+import config from "./config.json";
 
 ipcMain.on("filesSelected", (event, urls) => {
-    const outputPath = "D:/Conquiste sua Vaga/Projetos/renomear-arquivos/output";
+    const outputPath = config.outputPath;
 
     urls.forEach((url: string) => {
         const fileName = path.basename(url);
         const outputFile = path.join(outputPath, fileName);
+        console.log(`Copiando ${url}`)
 
         fs.copyFileSync(url, outputFile);
     })
