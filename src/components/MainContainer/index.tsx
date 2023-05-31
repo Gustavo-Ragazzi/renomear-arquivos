c7f00afddbaadc90d9702cf3ff0a10c34f44e68a
Gustavo Ragazzi <ragazzi@hotmail.com.br>
2023-05-31 18:42:44 -0300

    fix table layout

--- src/components/MainContainer/index.tsx
+++ src/components/MainContainer/index.tsx
@@ -5,26 +5,44 @@ const Container = styled.main`
     background-color: #424549;
     color: #c6c1c1;
     width: 50%;
-    padding: 0.5em 0;
 `
 
-const Item = styled.div`
-    display: flex;
-    justify-content: space-between;
-    padding: 0 0.5em;
+const Table = styled.table`
+    width: 100%;
+    border-collapse: collapse;
+`
+
+const TableRow = styled.tr`
     :hover {
         background-color: #616468;
     }
 `
 
+const FileNameCell = styled.td`
+    width: 45%;
+    padding: 0 0.5em;
+`
+
+const NewNameInputCell = styled.td`
+    width: 45%;
+    padding: 0 0.5em;
+`
+
+const ButtonCell = styled.td`
+    width: 10%;
+    padding: 0 0.5em;
+`
+
 const Button = styled.button`
-    align-self: center;
     font-size: 0.7em;
 `
 
 const FileName = styled.p`
-    align-self: center;
     margin: 0;
+`
+
+const NewNameInput = styled.input`
+    width: 100%;
 `
 
 export default function MainContainer(props: any) {
@@ -32,12 +50,23 @@ export default function MainContainer(props: any) {
 
     return (
         <Container>
-            {props.files.map((file: string) => (
-                <Item>
-                    <FileName key={file}>{file}</FileName>
-                    <Button type="button" className="btn-close" aria-label="Close" onClick={() => ipcRenderer.send("deleteFile", file)}></Button>
-                </Item>
-            ))}
+            <Table>
+                <tbody>
+                    {props.files.map((file: string) => (
+                        <TableRow key={file}>
+                            <FileNameCell>
+                                <FileName key={file + "-p"}>{file}</FileName>
+                            </FileNameCell>
+                            <NewNameInputCell>
+                                <NewNameInput />
+                            </NewNameInputCell>
+                            <ButtonCell>
+                                <Button type="button" className="btn-close" aria-label="Close" onClick={() => ipcRenderer.send("deleteFile", file)}></Button>
+                            </ButtonCell>
+                        </TableRow>
+                    ))}
+                </tbody>
+            </Table>
         </Container>
     )
 }
