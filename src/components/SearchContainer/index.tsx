2273a8fa97586747993d66154faaeb80f5528a1f
Gustavo Ragazzi <ragazzi@hotmail.com.br>
2023-05-31 19:01:37 -0300

    adding buttons for future updates

--- src/components/SearchContainer/index.tsx
+++ src/components/SearchContainer/index.tsx
@@ -1,13 +1,31 @@
 import { ipcRenderer } from "electron";
 import styled from "styled-components"
+import { CiImport, CiExport, CiEdit } from "react-icons/Ci"
 
-const Container = styled.form`
+const Container = styled.div`
     width: 50%;
     padding: 2em;
+`
+
+const AddFilesContainer = styled.form`
     height: 20px;
 `
 
+const ExpImpDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 2em;
+    margin: 3em 0;
+`
+
+const RenameContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 50%;
+`
+
 export default function SearchContainer() {
+
     const handleSubmit = (event: any) => {
         event.preventDefault();
         
@@ -17,10 +35,27 @@ export default function SearchContainer() {
         ipcRenderer.send("filesSelected", urls);
     };
 
+    function disableButtonAlert() {
+        alert("Funcionalidade ainda não implementada")
+    }
+
     return (
-        <Container className="input-group mb-3" onSubmit={handleSubmit}>
-            <input type="file" className="form-control" id="inputGroupFile02" name="fileInput" multiple/>
-            <button className="input-group-text" type="submit">Adicionar</button>
+        <Container>
+            <AddFilesContainer className="input-group mb-3" onSubmit={handleSubmit}>
+                <input type="file" className="form-control" id="inputGroupFile02" name="fileInput" multiple/>
+                <button className="input-group-text" type="submit">Adicionar</button>
+            </AddFilesContainer>
+            <ExpImpDiv>
+                <button onClick={disableButtonAlert} type="button" className="btn btn-primary">Exportar CSV<CiExport /></button>
+                <button onClick={disableButtonAlert} type="button" className="btn btn-primary">Importar CSV<CiImport /></button>
+            </ExpImpDiv>
+            <ExpImpDiv>
+                <button onClick={disableButtonAlert} type="button" className="btn btn-primary">Exportar JSON<CiExport /></button>
+                <button onClick={disableButtonAlert} type="button" className="btn btn-primary">Importar JSON<CiImport /></button>
+            </ExpImpDiv>
+            <RenameContainer>
+                <button type="button" className="btn btn-primary">Renomear<CiEdit /></button>
+            </RenameContainer>
         </Container>
     )
 }
